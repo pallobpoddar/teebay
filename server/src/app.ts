612d6bf012e65239aa0e4517f898d721e4b5f4a5
Pallob Poddar <pallobpoddar@gmail.com>
2025-02-13 08:54:34 +0600

    feat: setup logger with pino

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,6 +1,11 @@
 import express from "express";
 import database from "./config/database";
 import config from "./config";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import helmet from "helmet";
+import pinoHttp from "pino-http";
+import logger from "./utils/logger";
 
 class App {
   private app: express.Application;
@@ -14,19 +19,23 @@ class App {
   }
 
   private initializeMiddleware(): void {
+    this.app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+    this.app.use(helmet());
     this.app.use(express.json());
+    this.app.use(cookieParser());
+    this.app.use(pinoHttp());
   }
 
   public async start(): Promise<void> {
     try {
       await database.getInstance().connect();
-      console.log("Database connection established");
+      logger.info("Database connection established");
 
       this.app.listen(config.port, () => {
-        console.log(`Server is running on port ${config.port}`);
+        logger.info(`Server is running on port ${config.port}`);
       });
     } catch (error) {
-      console.error(error);
+      logger.error(error);
       process.exit(1);
     }
   }
