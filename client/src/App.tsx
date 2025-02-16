004e1db4e6e3ad68a3c4ee455d9ed25aebdb5c0a
Pallob Poddar <pallobpoddar@gmail.com>
2025-02-16 16:03:54 +0600

    feat: design Input in client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,6 @@
 import Button from "./components/atoms/Button";
 import DeleteIcon from "./assets/icons/delete-icon.svg?react";
+import Input from "./components/atoms/Input";
 
 function App() {
   return (
@@ -15,6 +16,17 @@ function App() {
       <Button onClick={() => {}}>
         <DeleteIcon />
       </Button>
+      <div className="p-4"></div>
+      <Input placeholder="Email" type="email" autocomplete="on" />
+      <div className="p-4"></div>
+      <Input
+        placeholder="Password"
+        type="password"
+        autocomplete="on"
+        includePasswordIcon
+      />
+      <div className="p-4"></div>
+      <Input placeholder="Password" type="password" autocomplete="on" />
     </>
   );
 }
---
+++ client/src/components/atoms/Input.tsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import VisibilityIcon from "../../assets/icons/visibility-icon.svg?react";
+import VisibilityOffIcon from "../../assets/icons/visibility-off-icon.svg?react";
+
+type Props = {
+  placeholder?: string;
+  type?: "text" | "email" | "password" | "datetime-local";
+  autocomplete?: string;
+  includePasswordIcon?: boolean;
+  field?: any;
+};
+
+const Input = (props: Props) => {
+  const [inputType, setInputType] = useState(props.type);
+
+  const togglePasswordVisibility = () => {
+    setInputType((prevType) => (prevType === "password" ? "text" : "password"));
+  };
+
+  return (
+    <div className="relative flex items-center">
+      <input
+        className="border-2 border-gray p-3 w-full rounded-sm"
+        type={inputType}
+        placeholder={props.placeholder}
+        autoComplete={props.autocomplete}
+        {...props.field}
+      />
+      {props.includePasswordIcon && (
+        <span
+          className="absolute right-3 cursor-pointer"
+          onClick={togglePasswordVisibility}
+        >
+          {inputType === "password" ? (
+            <VisibilityOffIcon />
+          ) : (
+            <VisibilityIcon />
+          )}
+        </span>
+      )}
+    </div>
+  );
+};
+
+export default Input;
